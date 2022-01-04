9be890181d7c86b68b6087efc880516674a40f2b
Praful Parashar <praful@urbanpiper.com>
2022-01-04 23:33:31 +0530

    - Added questions for the upcoming sprint

---
+++ Questions.py
@@ -0,0 +1,16 @@
+'''
+Q1. Armstrong number from 1-1000
+
+Q2. *               *****
+    **              ****
+    ***             ***
+    ****            **
+    *****           * 
+
+Make with iteration statements using for loops and if/else
+
+
+Q3. Shapes ke program mein ek triangle aur add karna hai and sab mein perimeter and area implement karna hai.
+
+
+'''
