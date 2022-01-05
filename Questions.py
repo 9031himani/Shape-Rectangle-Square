38473dec27cdea2c4c78dc88a6b3d587651c6bf5
9031himani <93124782+9031himani@users.noreply.github.com>
2022-01-05 21:51:19 +0530

    Update Questions.py

--- Questions.py
+++ Questions.py
@@ -13,4 +13,11 @@ Make with iteration statements using for loops and if/else
 Q3. Shapes ke program mein ek triangle aur add karna hai and sab mein perimeter and area implement karna hai.
 
 
+
+Q4. We have a list of tuples. Each tuple entry represents an item and a category combination.
+ We want to transform this data into a structure which allows us to retrieve all items of the category.
+
+ Input: [('Veg Biryani', 'Biryani'), ('Chicken Biryani', 'Biryani'), ('Egg Roll', 'Rolls'), ('Paneer wrap', 'Rolls'), ('Chicken Manchow Soup', 'Soup'), ('Egg Biryani', 'Biryani')]
+
+ Output: {'Biryani': ['Veg Biryani', 'Chicken Biryani', 'Egg Biryani'], 'Rolls': ['Egg Roll', 'Paneer wrap'], 'Soup': ['Chicken Manchow Soup ]}
 '''
